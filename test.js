ff43ff529ec3715f60347988b47d3ee9f7e5f7cb
Zuzanna1989 <z.kurtys@gmail.com>
2024-02-13 19:29:13 +0000

    moj pierwszy dodatek to team project

--- test.js
+++ test.js
@@ -2,3 +2,7 @@ console.log("version22222 poprawki");
 console.log("version22222 poprawki");
 console.log("version22222 poprawki");
 console.log("hahahahahahahah122222lololo-------nowa wersja");
+console.log("version22222 poprawki");
+console.log("version22222 poprawki");
+console.log("version22222 poprawki");
+console.log("CONTRIBUTING TO TEAM PROJECT");
